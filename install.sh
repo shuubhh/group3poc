6a241085ab578e19f6c4cc01aa7fac3ad8c05ef9
Shubh <64408647+shuubhh@users.noreply.github.com>
2025-09-12 16:12:13 +0530

    Update00 install.sh

--- install.sh
+++ install.sh
@@ -12,17 +12,17 @@ sudo mkdir -p $APP_DIR
 sudo rm -rf $APP_DIR/*
  
 echo "🔹 Copying build artifacts..."
-cp -r * $APP_DIR
-cd $APP_DIR
+sudo cp -r * $APP_DIR
+sudo cd $APP_DIR
  
 echo "🔹 Installing dependencies..."
-npm install
+sudo npm install
  
 echo "🔹 Starting app with PM2..."
 sudo npm install -g pm2
-pm2 start server.js --name helloapp || pm2 restart helloapp
-pm2 startup systemd -u $USER --hp $HOME
-pm2 save
+sudo pm2 start server.js --name helloapp || sudo pm2 restart helloapp
+sudo pm2 startup systemd -u $USER --hp $HOME
+sudo pm2 save
  
 echo "✅ Deployment complete. App running on port 80."
  
