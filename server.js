893db7769ca2014057a90c28a8e71755c43173ff
Shubh <64408647+shuubhh@users.noreply.github.com>
2025-09-16 14:28:33 +0530

    Updateport3000 server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const http = require("http");
 const hostname = "0.0.0.0";
-const port = 80;
+const port = process.env.PORT || 3000;
  
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World from SSTP!\n");
+  res.end("Hello World from SSTPv3!\n");
 });
  
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 
 });
+
