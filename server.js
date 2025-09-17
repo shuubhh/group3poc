ac4bb8a239ddd5ae896cd4c76e66e1336b1630c5
Shubh <64408647+shuubhh@users.noreply.github.com>
2025-09-17 19:13:47 +0530

    Updatev53001 server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const http = require("http");
 const hostname = "0.0.0.0";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
  
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World from SSTPv4!\n");
+  res.end("Hello World from SSTPv5!\n");
 });
  
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 
 });
+
 
 
