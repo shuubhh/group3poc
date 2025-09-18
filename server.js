7aead03980867f42804e5197c44f802d9f69a2bf
Shubh <64408647+shuubhh@users.noreply.github.com>
2025-09-18 19:22:23 +0530

    Update v6 server.js

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const port = process.env.PORT || 3001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World from SSTPv5!\n");
+  res.end("Hello World v6!\n");
 });
  
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 
 });
+
 
 
 
