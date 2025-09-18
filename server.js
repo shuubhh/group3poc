a98724ef24c25a8259eee4095925e6d71e04b754
Shubh <64408647+shuubhh@users.noreply.github.com>
2025-09-18 19:42:30 +0530

    Update v7 server.js

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const port = process.env.PORT || 3001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World v6!\n");
+  res.end("Hello World v7!\n");
 });
  
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 
 });
+
 
 
 
